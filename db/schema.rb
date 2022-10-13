fd8748c108ba15f045271ff25bd04c15c4b6d4cd
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-13 08:00:03 +0000

    [Add]カラムの追加コマンド実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_070021) do
+ActiveRecord::Schema.define(version: 2022_10_13_075906) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title　"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
