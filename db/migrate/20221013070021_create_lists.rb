1d8cad5d15088c4bdd7f32c7ee180f664c3a1134
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-13 07:57:45 +0000

    [Add]migrationファイルをデータベースへ反映し、テーブルを作成する

--- db/migrate/20221013070021_create_lists.rb
+++ db/migrate/20221013070021_create_lists.rb
@@ -1,7 +1,8 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-
+      t.string :title　# ブログのタイトル
+      t.string :body   # ブログの本文
       t.timestamps
     end
   end
